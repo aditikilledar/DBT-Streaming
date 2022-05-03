23b6c3be8720d4ce9c7ec78d46512d18080ed8de
Chinmayee <55998927+chinmayeeet@users.noreply.github.com>
2022-05-03 21:24:48 +0530

    Update client.py

--- client.py
+++ client.py
@@ -14,9 +14,21 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext, DStream
 from pyspark.sql import SQLContext, Row, SparkSession
 
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+import json
+from json import dumps
+
+
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
+
+
 from collections import namedtuple
 
-
+"""
 # config
 sc = SparkContext("local[2]", "NetworkWordCount")
 spark = SparkSession(sc)
@@ -65,3 +77,46 @@ while count < 5:
 # stop streaming and wait couple of minutes to get enought tweets
 # ssc.stop()
 ssc.awaitTermination()
+
+"""
+
+def send_to_kafka(rows):
+   
+        word = rows[0]
+        count = rows[1]
+        print(rows)
+        print(type(word))
+        producer = KafkaProducer(
+        bootstrap_servers=['localhost:9092'], api_version=(0, 10, 1), value_serializer=lambda x: 
+                         dumps(x).encode('utf-8'))
+
+        
+        v= {rows[0]:rows[1]}
+        if(word == 'IPL' or word == "TATAIPL" or word == 'LSG' or word == 'PBKS'):
+            producer.send(word,value=v)
+            print("============================got a tweet==================================");
+            
+
+
+if __name__ == "__main__":
+    
+    sc = SparkContext("local[2]", "NetworkWordCount")
+    ssc = StreamingContext(sc, 60)
+    lines = ssc.socketTextStream("localhost", 5556)
+    words = lines.flatMap(lambda line: line.split(" "))
+    pairs = words.map(lambda word: (word, 1))
+    wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+    wordCounts.foreachRDD(lambda rdd: rdd.foreach(send_to_kafka))
+    ssc.start()            
+    ssc.awaitTermination()
+    
+
+def on_send_success(record_metadata):
+    print(record_metadata.topic)
+    print(record_metadata.partition)
+    print(record_metadata.offset)
+
+
+def on_send_error(excp):
+    log.error('I am an errback', exc_info=excp)
+        
