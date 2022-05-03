2cff16cbc6a1d0db603904a5db151f4ea3b79d6e
Chinmayee <55998927+chinmayeeet@users.noreply.github.com>
2022-05-03 22:43:27 +0530

    Create readtweets.py

---
+++ final/readtweets.py
@@ -0,0 +1,20 @@
+from kafka import KafkaConsumer
+from json import loads;
+from pymongo import MongoClient
+
+def write(message):
+    client = MongoClient()
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.dbt
+    d = db["kpop"];
+    d.insert_one(message);
+
+# To consume latest messages and auto-commit offsets
+consumer = KafkaConsumer('NCT',
+                         group_id='my-group',
+                         bootstrap_servers=['localhost:9092'], value_deserializer=lambda x: loads(x.decode('utf-8')))
+for message in consumer:
+    message = message.value
+    write(message)
+    print('{} added to '.format(message))
+
